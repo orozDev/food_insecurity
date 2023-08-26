dab12d584fa303b50f539040c47ca184570ac157
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2023-08-26 15:21:49 +0600

    ..

--- api/auth/endpoints.py
+++ api/auth/endpoints.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from . import api
+
+router = routers.DefaultRouter()
+router.register('users', api.UserViewSet)
 
 urlpatterns = [
     path('profile/', api.ProfileApiView.as_view()),
     path('login/', api.LoginApi.as_view()),
     path('register/', api.RegisterApi.as_view()),
+    path('', include(router.urls)),
 ]
